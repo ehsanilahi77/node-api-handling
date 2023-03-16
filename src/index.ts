028cebf213fac7acfd297092025c8b1babbbf74a
ehsan-ilahi <ehsanilahi77@gmail.com>
2023-03-16 13:08:20 +0500

    update-fixe

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+// const app = require('./server')
+import app from './server'
+
+app.listen(3001, ()=> {
+    console.log('http://localhost:3001')
+})
---
+++ src/server.ts
@@ -0,0 +1,34 @@
+// const express = require('express')
+import express from 'express'
+import router from './router'
+import morgan from 'morgan'
+import cors from 'cors'
+
+const app = express()
+
+const customLogger = (message) => (req, res, next) => {
+    console.log(`Hello from ${message}`)
+    next()
+}
+
+app.use(cors())
+app.use(morgan('dev'))
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.use(customLogger('customer Logger'))
+
+/*app.use((req, res, next) =>{
+    req.test = 'testing'
+    next()
+})*/
+
+app.get('/', (req, res)=>{
+    console.log('hello from express')
+    res.status(200)
+    res.json({message : 'hello'})
+})
+
+app.use('/api',router)
+
+// module.exports = app
+export default app
